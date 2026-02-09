3f2e644c075c0ed2299305a325c969a8700acb35
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2026-02-09 14:55:42 -0500

    docs(docs-infra): remove zone change detection from forms example
    Removes the `provideZoneChangeDetection` provider from the bootstrap application configuration in the forms examples

--- adev/src/content/examples/form-validation/src/main.ts
+++ adev/src/content/examples/form-validation/src/main.ts
@@ -1,8 +1,5 @@
 // #docregion
 import {bootstrapApplication} from '@angular/platform-browser';
 import {AppComponent} from './app/app.component';
-import {provideZoneChangeDetection} from '@angular/core';
 
-bootstrapApplication(AppComponent, {
-  providers: [provideZoneChangeDetection({eventCoalescing: true})],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent).catch((err) => console.error(err));
